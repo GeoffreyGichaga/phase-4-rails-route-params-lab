929f6809dc548544958a02fe3fd05b8bb043b013
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-07-20 02:31:09 +0300

    commit tree 78e102cbc2c84d702510bba1d287576a2115a1f6

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,23 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = 
+    if params[:name]
+      Student.by_name(params[:name])
+    else
+      Student.all
+    end 
     render json: students
   end
+
+  def find_student
+    student = Student.find(params[:id])
+    render json: student
+  end 
+
+  def student_by_name
+    student = Student.find(params[:name])
+    render json: student
+  end 
 
 end
